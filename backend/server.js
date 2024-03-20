02e506b9a95e8262b7e8f06dc09a2d1003ef198c
yateeshuu <you@example.com>
2024-03-20 11:13:38 +0530

    vercel changes

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,10 @@ const port = process.env.PORT || 5000;
 
 // MySQL connection
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'code_snippets_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 connection.connect((err) => {
